5860359f5316635c7bc7b12885addd2ddc86959f
FabianPinoP <fpinoperez@outlook.cl>
2023-01-13 18:13:59 -0300

    add instructions in readme change enviroment variables

--- readme.md
+++ readme.md
@@ -10,3 +10,10 @@ pinchar en "Only select repositories"
 
 seleccionar el repo a deployar
 
+configurar variables de entorno en el proyecto
+
+crear proyecto postgres en railway
+entrar al proyecto pinchar en variables
+agregar variables de entorno base de datos al proyecto
+
+agregar script a package,json  "start": "node app.js",
--- src/db/connectionDb.js
+++ src/db/connectionDb.js
@@ -2,11 +2,11 @@ const { Pool } = require("pg");
 require('dotenv').config()
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.HOST,
-    password: process.env.PASS,
-    database: process.env.DATABASE,
-    port: 5432,
+    user: process.env.DB_USER || "fabian",
+    host: process.env.HOST || "localhost",
+    password: process.env.PASS || "papaya",
+    database: process.env.DATABASE || "registers",
+    port: process.env.DB_PORT || 5432,
     allowExitOnIdle: true
 });
 
